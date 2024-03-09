31fe8d2c8b9e73eb017a7c3af3f8be706e17d966
KhoiBui <156224787+KhoiBui16@users.noreply.github.com>
2024-03-09 10:16:07 +0700

    source

--- Python/Source Code Python Contest/Contest_2_VongLap/Bai22-VeHinh2.py
+++ Python/Source Code Python Contest/Contest_2_VongLap/Bai22-VeHinh2.py
@@ -1,45 +1,4 @@
 n = int(input())
-# sua bai cach 2:
-# hinh 1:
-for i in range(1, n + 1):
-    for j in range(i):
-        print('*', end = '')
-    print()
-print()
-# hinh 2:
-for i in range(n, 0, -1):
-    for j in range(i):
-        print('*', end = '')
-    print()
-print()
-# hinh 3:
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        if j <= n - i:
-            print(' ', end = '')
-        else:
-            print('*', end = '')
-    print()
-print()
-# hinh 4:
-for i in range(1, n + 1):
-    for j in range(1, n + 1):
-        if j < i:
-            print(' ', end = '')
-        else:
-            print('*', end = '')
-    print()
-# hinh 5:
-for i in range(1, n + 1):
-    for j in range(1, i + 1):
-        if i == 1 or i == n or i == j or j == 1:
-            print('*', end = '')
-        else:
-            print(' ', end = '');
-    print()
-'''
-    Code by anh khoi
-    n = int(input())
 for i in range(n):
     for j in range(i + 1):
         print('*', end = '')
@@ -71,4 +30,3 @@ for i in range(n):
         else:
             print(' ', end = '')
     print()
-'''
