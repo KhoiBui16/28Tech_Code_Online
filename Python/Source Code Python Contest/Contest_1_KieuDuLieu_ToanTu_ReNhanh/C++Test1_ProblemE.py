df274e60eaca20ef3bcd1bcbb6086fd466e7fdd1
KhoiBui <156224787+KhoiBui16@users.noreply.github.com>
2024-03-09 10:03:48 +0700

    source

--- Python/Source Code Python Contest/Contest_1_KieuDuLieu_ToanTu_ReNhanh/C++Test1_ProblemE.py
+++ Python/Source Code Python Contest/Contest_1_KieuDuLieu_ToanTu_ReNhanh/C++Test1_ProblemE.py
@@ -2,9 +2,13 @@ import math
 a, b, c = map(int, input().split())
 if a == 0:
     if b == 0:
-        print('VO SO NGHIEM')
-    x = -c / b
-    print('%.2f' % x)
+        if c == 0:
+            print('VO SO NGHIEM')
+        else:  
+            print('VO NGHIEM')
+    else:
+        x = -c / b
+        print('%.2f' % x)
 else:
     delta = b ** 2 - 4 * a * c
     if delta > 0:
@@ -12,7 +16,7 @@ else:
         x2 = (-b - math.sqrt(delta)) / (2 * a)
         print('%.2f' % x2, '%.2f' % x1, sep = ' ')
     elif delta == 0:
-        x= -b / 2 * a
+        x= -b / (2 * a)
         print('%.2f' % x)
     else:
-        print('VO NGHIEM')
+        print('VO NGHIEM')              
