137415b5da43b18358f7e0154902af150666cada
KhoiBui <156224787+KhoiBui16@users.noreply.github.com>
2024-03-09 10:44:16 +0700

    source

--- Python/Source Code Python Contest/Contest_3_HamVaLyThuyetSo/Bai23_TongUocSo.py
+++ Python/Source Code Python Contest/Contest_3_HamVaLyThuyetSo/Bai23_TongUocSo.py
@@ -2,10 +2,10 @@ import math
 
 def sum_of_divisors(n):
     sum = 0
-    for i in range(2, math.isqrt(n) + 1):
+    for i in range(1, math.isqrt(n) + 1):
         if n % i == 0:
-            sum += i
-            if i != n // i:
+            sum += i 
+            if i != (n // i):
                 sum += n // i
     return sum
 
